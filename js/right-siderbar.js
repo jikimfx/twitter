f307b4e6e7a7991da4f48bae7ab2394d3cd4a98f
Minh Nguyen <gin411@Minhs-MacBook-Pro-2.local>
2020-05-23 00:39:22 -0400

    Right sidebar

--- js/right-siderbar.js
+++ js/right-siderbar.js
@@ -1,3 +1,57 @@
+let newsList =[]
+let pageNumber=1;
+const apiKey = "0772b01374944094a2c6a39147e609b1"
+const loadNews = async() =>{
+    let url = `http://newsapi.org/v2/everything?domains=wsj.com&apiKey=${apiKey}`
+    let data =  await fetch(url)
+    let result =  await data.json();
+    let resultList = result.articles
+    newsList=newsList.concat(resultList)
+    console.log("length of news",newsList.length)
+    render(newsList)
+   
+}
+const render = (list) =>{
+  console.log("you call render list", list)
+     
+  let newsHtml = newsList.map(item =>{
+     console.log(item.publishedAt)
+  `<div id="news">
+  <div id="contentsArea">
+     <div style="font-size: 25px;"id="title">${item.title}</div>
+     
+     <div id="source">${item.source.name}</div>
+    
+     <div class="row-mb5" style="font-size: 12px;">${moment(item.publishedAt).startOf('hour').fromNow()}</div>
+     
+     
+     
+  </div>
+  <div id="imgArea">
+            
+  </div>
+ 
+  
+  
+</div>
+
+`
+  }).join('')
+
+
+
+document.getElementById("news").innerHTML = newsHtml
+
+}
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
     // executes when HTML-Document is loaded and DOM is ready
    console.log("document is ready");
@@ -13,3 +67,15 @@ $(document).ready(function() {
      
    // document ready  
    });
+
+
+
+   $('.share-btn').click(function(){
+    $(this).addClass("clicked");
+});
+$('.close').click(function (e) {
+  $('.clicked').removeClass('clicked');
+  e.stopPropagation();
+});  
+   
+
